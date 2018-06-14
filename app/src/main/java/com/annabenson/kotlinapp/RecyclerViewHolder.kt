60a750d8029b7d3591fed33a985d8d19948d1cd3
annalbenson <annalucybenson@gmail.com>
2018-06-14 12:55:04 -0500

    LT->Gt Converting Java ViewHolder to Kotlin ViewHolder

--- app/src/main/java/com/annabenson/kotlinapp/RecyclerViewHolder.kt
+++ app/src/main/java/com/annabenson/kotlinapp/RecyclerViewHolder.kt
@@ -2,9 +2,23 @@ package com.annabenson.kotlinapp
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.TextView
 
-class RecyclerViewHolder : RecyclerView.ViewHolder(val v : View) {
 
+class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+    var name: TextView
+    var symbol: TextView
+    var price: TextView
+    var change: TextView
+
+    init {
+        // do
+
+        name = view.findViewById(R.id.nameID)
+        symbol = view.findViewById(R.id.symbolID)
+        price = view.findViewById(R.id.priceID)
+        change = view.findViewById(R.id.changeID)
+    }
 
 }
